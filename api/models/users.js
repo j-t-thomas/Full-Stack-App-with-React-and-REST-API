a85c75ef15d3a442ceb1128a18496bcb9650915e
“j-t-thomas” <janethomas1296@gmail.com>
2024-02-16 12:51:06 -0800

    Updated user model in API and UserSignUp

--- api/models/users.js
+++ api/models/users.js
@@ -56,9 +56,11 @@ module.exports = (sequelize) => {
                 },
             },
             set(val) {
-                const hashedPassword = bcrypt.hashSync(val, 10);
-                this.setDataValue('password', hashedPassword);
-            },
+                if (val) {
+                    const hashedPassword = bcrypt.hashSync(val, 10);
+                    this.setDataValue('password', hashedPassword);
+                }
+            }
         },
 
 
