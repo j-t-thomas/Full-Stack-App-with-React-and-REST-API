f97d79d654ae386940063077f4bf61cefdde1f07
“j-t-thomas” <janethomas1296@gmail.com>
2024-02-15 21:23:32 -0800

    Header with Validation errors apprearing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,31 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import UserContext from '../context/UserContext';
 
-//COMPONENT
 const Header = () => {
   const { authUser } = useContext(UserContext);
 
-//PAGE RENDERING
   return (
-    <header>
-      <div className='wrap header--flex'>
-        <h1 className='header--logo'><Link to='/'>Courses</Link></h1>
-        <nav>
-          {authUser === null?
-          <>
-            <ul className='header--signedout'>
-              <li><Link to='/signup'>Sign up</Link></li>
-              <li><Link to='/signin'>Sign in</Link></li>
-            </ul>
-          </>
-          :
-          <>
-            <ul className='header--signedin'>
-            <li>Welcome, {authUser.firstName} {authUser.lastName}!</li>
-              <li><Link to='/signout'>Sign out</Link></li>
-            </ul>
-          </>
-        }
-        </nav>
-      </div>
-    </header>
+      <header>
+          <div className="wrap header--flex">
+              <h1 className="header--logo"><a href="/">Courses</a></h1>
+              <nav>
+                  <ul className="header--signedout">
+                  { authUser === null ?
+                  <>
+                      <li><a href="/signup">Sign Up</a></li>
+                      <li><a href="/signin">Sign In</a></li>
+                  </>
+                  :
+                  <>
+                      <span>Welcome {authUser.firstName}</span>
+                      <li><a href="/signout">Sign Out</a></li>
+                  </>
+                  }
+                  </ul>
+              </nav>
+          </div>
+      </header>
   );
 };
 
